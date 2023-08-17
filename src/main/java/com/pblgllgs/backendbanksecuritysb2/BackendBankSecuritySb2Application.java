87712bee015365ba2c0b0e61af1fa4238e23082f
pbl.gllgs <pbl.gllgs@gmail.com>
2023-08-17 16:12:13 -0400

    exclude UserDetailsServiceAutoConfiguration

--- src/main/java/com/pblgllgs/backendbanksecuritysb2/BackendBankSecuritySb2Application.java
+++ src/main/java/com/pblgllgs/backendbanksecuritysb2/BackendBankSecuritySb2Application.java
@@ -2,9 +2,10 @@ package com.pblgllgs.backendbanksecuritysb2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = UserDetailsServiceAutoConfiguration.class)
 @EnableWebSecurity(debug = true)
 public class BackendBankSecuritySb2Application {
 
