2ea0890e92e1d845795c8bd49f4ae59aa0093d26
pbl.gllgs <pbl.gllgs@gmail.com>
2023-08-17 16:18:41 -0400

    add cors new implementation

--- src/main/java/com/pblgllgs/backendbanksecuritysb2/config/SecurityFilterChainConfig.java
+++ src/main/java/com/pblgllgs/backendbanksecuritysb2/config/SecurityFilterChainConfig.java
@@ -1,17 +1,21 @@
 package com.pblgllgs.backendbanksecuritysb2.config;
 
-import com.pblgllgs.backendbanksecuritysb2.filters.AuthoritiesLoggingAfterFilter;
-import com.pblgllgs.backendbanksecuritysb2.filters.AuthoritiesLoggingAtFilter;
-import com.pblgllgs.backendbanksecuritysb2.filters.RequestValidationBeforeFilter;
+import com.pblgllgs.backendbanksecuritysb2.filters.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
-import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Arrays;
+import java.util.Collections;
 
 
 @Configuration
@@ -21,9 +25,12 @@ public class SecurityFilterChainConfig {
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .cors(Customizer.withDefaults())
-                .csrf().ignoringAntMatchers("/api/contact")
-                .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
-                .and()
+                .csrf().disable()
+                .addFilterBefore(new RequestValidationBeforeFilter(), BasicAuthenticationFilter.class)
+                .addFilterBefore(new JWTTokenValidatorFilter(), BasicAuthenticationFilter.class)
+                .addFilterAt(new AuthoritiesLoggingAtFilter(), BasicAuthenticationFilter.class)
+                .addFilterAfter(new AuthoritiesLoggingAfterFilter(), BasicAuthenticationFilter.class)
+                .addFilterAfter(new JWTTokenGeneratorFilter(), BasicAuthenticationFilter.class)
                 .authorizeHttpRequests(auth -> auth
                         .antMatchers("/api/myAccount").hasRole("USER")
                         .antMatchers("/api/myBalance").hasAnyRole("USER", "ADMIN")
@@ -32,9 +39,8 @@ public class SecurityFilterChainConfig {
                         .antMatchers("/api/user").authenticated()
                         .antMatchers("/api/notices", "/api/contact").permitAll()
                 )
-                .addFilterBefore(new RequestValidationBeforeFilter(), BasicAuthenticationFilter.class)
-                .addFilterAt(new AuthoritiesLoggingAtFilter(), BasicAuthenticationFilter.class)
-                .addFilterAfter(new AuthoritiesLoggingAfterFilter(), BasicAuthenticationFilter.class)
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
                 .httpBasic(Customizer.withDefaults());
         return http.build();
 
