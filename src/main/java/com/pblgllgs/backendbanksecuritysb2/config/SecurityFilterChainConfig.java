84bc88dc4d278c83348978d325ca7719fe1c6b27
pbl.gllgs <pbl.gllgs@gmail.com>
2023-08-20 01:07:25 -0400

    delete unused imports

--- src/main/java/com/pblgllgs/backendbanksecuritysb2/config/SecurityFilterChainConfig.java
+++ src/main/java/com/pblgllgs/backendbanksecuritysb2/config/SecurityFilterChainConfig.java
@@ -10,12 +10,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-
-import javax.servlet.http.HttpServletRequest;
-import java.util.Arrays;
-import java.util.Collections;
 
 
 @Configuration
