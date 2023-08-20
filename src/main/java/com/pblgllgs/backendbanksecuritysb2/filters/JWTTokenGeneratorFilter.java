27eeb6e380dcaa3039f0c1b7dc3148f9eb2dd80c
pbl.gllgs <pbl.gllgs@gmail.com>
2023-08-20 01:08:01 -0400

    delete slf4j comment

--- src/main/java/com/pblgllgs/backendbanksecuritysb2/filters/JWTTokenGeneratorFilter.java
+++ src/main/java/com/pblgllgs/backendbanksecuritysb2/filters/JWTTokenGeneratorFilter.java
@@ -1,5 +1,19 @@
 package com.pblgllgs.backendbanksecuritysb2.filters;
 
+import com.pblgllgs.backendbanksecuritysb2.constants.SecurityConstants;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.crypto.SecretKey;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.util.Collection;
@@ -7,45 +21,27 @@ import java.util.Date;
 import java.util.HashSet;
 import java.util.Set;
 
-import javax.crypto.SecretKey;
-import javax.servlet.FilterChain;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import com.pblgllgs.backendbanksecuritysb2.constants.SecurityConstants;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.filter.OncePerRequestFilter;
-
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.security.Keys;
-
 @Slf4j
 public class JWTTokenGeneratorFilter extends OncePerRequestFilter {
-
 
     @Override
     public void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
             throws IOException, ServletException {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        log.info(String.format("username %s",authentication.getName()));
         if (null != authentication) {
             SecretKey key = Keys.hmacShaKeyFor(SecurityConstants.JWT_KEY.getBytes(StandardCharsets.UTF_8));
-            String jwt = Jwts.builder().setIssuer("JWT").setSubject(authentication.getName())
+            String jwt = Jwts.builder().setIssuer("pblgllgs").setSubject("JWT Token")
                     .claim("username", authentication.getName())
                     .claim("authorities", populateAuthorities(authentication.getAuthorities()))
                     .setIssuedAt(new Date())
-                    .setExpiration(new Date((new Date()).getTime() + 300000000))
+                    .setExpiration(new Date((new Date()).getTime() + 30000000))
                     .signWith(key).compact();
             response.setHeader(SecurityConstants.JWT_HEADER, jwt);
-            log.info(jwt);
         }
 
         chain.doFilter(request, response);
     }
+
 
     @Override
     protected boolean shouldNotFilter(HttpServletRequest request) {
